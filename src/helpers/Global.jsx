feda5d2349f71c1f356909d7e052617f94292ce9
JuanjoTorre <jjtorregalan@gmail.com>
2025-10-21 21:53:33 +0200

    Ultima

--- src/helpers/Global.jsx
+++ src/helpers/Global.jsx
@@ -3,5 +3,5 @@
 
 export const Global = {
     // url: "https://honyaldi-backend-10.railway.internal/api"
-    url:"honyaldi-backend-10-production.up.railway.app/api"
+    url:"https://honyaldi-backend-10-production.up.railway.app/api"
 }
