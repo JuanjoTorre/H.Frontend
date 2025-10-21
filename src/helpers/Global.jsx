c83d1bd138da6c358370581f1b6d5b5147678693
JuanjoTorre <jjtorregalan@gmail.com>
2025-10-21 21:37:06 +0200

    Prueba 21/10/25

--- src/helpers/Global.jsx
+++ src/helpers/Global.jsx
@@ -2,5 +2,6 @@
  * ********************************************************************* */
 
 export const Global = {
-    url: "https://honyaldi-backend-10.railway.internal/api"
+    // url: "https://honyaldi-backend-10.railway.internal/api"
+    url:"honyaldi-backend-10-production.up.railway.app/api"
 }
