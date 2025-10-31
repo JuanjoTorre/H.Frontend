3d6b5460baa0222aa37ebe30bd10c47e7cc23f97
JuanjoTorre <jjtorregalan@gmail.com>
2025-10-31 20:49:35 +0100

    Arreglos de imagenes2

--- src/helpers/Global.jsx
+++ src/helpers/Global.jsx
@@ -2,6 +2,6 @@
  * ********************************************************************* */
 
 export const Global = {
-    url: "http://localhost:3900/api/"
-    // url:"https://honyaldi-backend-10-production.up.railway.app/api/"
+    // url: "http://localhost:3900/api/"
+    url:"https://honyaldi-backend-10-production.up.railway.app/api/"
 }
